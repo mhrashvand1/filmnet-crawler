d6a420309cb17a85d3eabb8f017428238ae0faf4
mohammadhoseinrashvand <mohammadhoseinrashvand@gmail.com>
2023-04-06 17:36:10 +0330

    Add MovieItem, Add Custom ImagesPipeline to change the storage address based on movie id and image type (cover/poster)

--- crawler/crawler/items.py
+++ crawler/crawler/items.py
@@ -1,12 +1,29 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
-class CrawlerItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class MovieItem(scrapy.Item):
+    
+    id = scrapy.Field()
+    short_id = scrapy.Field()
+    slug = scrapy.Field()
+    fa_title = scrapy.Field()
+    en_title = scrapy.Field()
+    summary = scrapy.Field()
+    publish_date = scrapy.Field()
+    release_year = scrapy.Field()
+    rate = scrapy.Field()
+    duration = scrapy.Field()
+    
+    # The url of the cover and poster image in the filmnet
+    cover_image_url = scrapy.Field()
+    poster_image_url = scrapy.Field()
+    
+    # The url of the downloaded poster and cover image
+    cover_image = scrapy.Field()
+    poster_image = scrapy.Field()
+    
+    # List of the genres
+    genres = scrapy.Field()
+    
+    # filmnet_link: This field is a combination of slug and short_id and does not need to be taken separately, 
+    # but this field is created in the serializer that is used to display the movie in the Django project.
--- crawler/crawler/pipelines.py
+++ crawler/crawler/pipelines.py
@@ -1,13 +1,46 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
+from scrapy.pipelines.images import ImagesPipeline as BaseImagesPipeline
 from itemadapter import ItemAdapter
+import hashlib
+from scrapy.utils.python import to_bytes
+from scrapy.http.request import Request, NO_CALLBACK
+from contextlib import suppress
 
 
-class CrawlerPipeline:
-    def process_item(self, item, spider):
+class ImagesPipeline(BaseImagesPipeline):
+    
+    """
+    Custom ImagesPipeline
+        Generate the storage address based on Movie ID and image type
+    """
+    
+    def file_path(self, request, response=None, info=None, *, item=None):
+        image_guid = hashlib.sha1(to_bytes(request.url)).hexdigest()
+        movie_id, image_type = request.meta['movie_id'], request.meta['image_type']
+        return f"{movie_id}/full/{image_type+image_guid}.jpg"
+
+    def get_media_requests(self, item, info):
+        item = ItemAdapter(item)
+        movie_id = item.get('id')
+        return [
+            Request(
+                item.get('cover_image_url'),
+                callback=NO_CALLBACK, 
+                meta={'movie_id':movie_id, 'image_type':'cover'}
+            ),
+            Request(
+                item.get('poster_image_url'),
+                callback=NO_CALLBACK, 
+                meta={'movie_id':movie_id, 'image_type':'poster'}
+            ),   
+        ]
+
+    def item_completed(self, results, item, info):
+        with suppress(KeyError):
+            ItemAdapter(item)['cover_image'] = results[0]
+            ItemAdapter(item)['poster_image'] = results[1]
         return item
+
+
+# class CrawlerPipeline: 
+#     def process_item(self, item, spider):
+#         return item
